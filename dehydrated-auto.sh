597e2702b77be1b1a81f78cf6cf6ecb27e605096
Brian Q <bqq100@gmail.com>
2016-11-15 13:06:56 -0500

    - Added daemon ability for dehydrated-auto.sh.  Will run every 10-12 hours so we don't always hit CA at the same time - Added a sample service file for systemd

--- dehydrated-auto.sh
+++ dehydrated-auto.sh
@@ -42,25 +42,39 @@ if [ $# = 1 ]; then
     fi
 fi
 
-# If no parameters specify, default to "-c -g", otherwise run with passed in parameters
-if [ $# = 0 ]; then
-    $SCRIPTDIR/dehydrated -c -g 
+function execute {
+    # If no parameters specify, default to "-c -g", otherwise run with passed in parameters
+    if [ $# = 0 ]; then
+        $SCRIPTDIR/dehydrated -c -g 
+    else
+        $SCRIPTDIR/dehydrated $@
+    fi
+
+    # Ensure proper permissions
+    find -L $SCRIPTDIR -type d -exec chmod 755 {} \;
+    find -L $CERTDIR -type d -exec chmod 755 {} \;
+    find -L $CERTDIR -name 'cert-*pem' -exec chmod 444 {} \;
+    find -L $CERTDIR -name 'chain-*pem' -exec chmod 444 {} \;
+    find -L $CERTDIR -name 'fullchain-*pem' -exec chmod 444 {} \;
+    find -L $CERTDIR -name 'cert-*csr' -exec chmod 400 {} \;
+    find -L $CERTDIR -name 'privkey-*pem' -exec chmod 400 {} \;
+
+    # Archive old certificates
+    $SCRIPTDIR/dehydrated -gc;
+
+    # Delete really old certficates
+    find -L $SCRIPTDIR/archive -name '*.pem' -mtime 365 -type f -exec rm -f {} \;
+    find -L $SCRIPTDIR/archive -name '*.csr' -mtime 365 -type f -exec rm -f {} \;
+}
+
+# Should we daemonize?
+if [ "$1" = "-d" -o "$1" = "--daemon" ]; then
+    shift;
+    while [ 1 ]; do
+        execute $@
+        SLEEP=$(($RANDOM % 120 + 600));
+        sleep $SLEEP"m" 
+    done
 else
-    $SCRIPTDIR/dehydrated $@
+    execute $@
 fi
-
-# Ensure proper permissions
-find -L $SCRIPTDIR -type d -exec chmod 755 {} \;
-find -L $CERTDIR -type d -exec chmod 755 {} \;
-find -L $CERTDIR -name 'cert-*pem' -exec chmod 444 {} \;
-find -L $CERTDIR -name 'chain-*pem' -exec chmod 444 {} \;
-find -L $CERTDIR -name 'fullchain-*pem' -exec chmod 444 {} \;
-find -L $CERTDIR -name 'cert-*csr' -exec chmod 400 {} \;
-find -L $CERTDIR -name 'privkey-*pem' -exec chmod 400 {} \;
-
-# Archive old certificates
-$SCRIPTDIR/dehydrated -gc;
-
-# Delete really old certficates
-find -L $SCRIPTDIR/archive -name '*.pem' -mtime 365 -type f -exec rm -f {} \;
-find -L $SCRIPTDIR/archive -name '*.csr' -mtime 365 -type f -exec rm -f {} \;
